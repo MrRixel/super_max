6ea3bb670900f9ea974f8d0a23865b48ec4170c9
Em30-tm-lua <emcteam2016@gmail.com>
2016-03-04 22:18:14 +0330

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -127,7 +127,7 @@ local function run(msg, matches)
   end
 
   -- Re-enable a plugin for this chat
-  if matches[1] == 'enable' and matches[3] == 'chat' then
+  if matches[1] == '+' and matches[3] == 'chat' then
     local receiver = get_receiver(msg)
     local plugin = matches[2]
     print("enable "..plugin..' on this chat')
@@ -135,14 +135,14 @@ local function run(msg, matches)
   end
 
   -- Enable a plugin
-  if matches[1] == 'enable' then
+  if matches[1] == '+' then
     local plugin_name = matches[2]
     print("enable: "..matches[2])
     return enable_plugin(plugin_name)
   end
 
   -- Disable a plugin on a chat
-  if matches[1] == 'disable' and matches[3] == 'chat' then
+  if matches[1] == '-' and matches[3] == 'chat' then
     local plugin = matches[2]
     local receiver = get_receiver(msg)
     print("disable "..plugin..' on this chat')
@@ -150,13 +150,13 @@ local function run(msg, matches)
   end
 
   -- Disable a plugin
-  if matches[1] == 'disable' then
+  if matches[1] == '-' then
     print("disable: "..matches[2])
     return disable_plugin(matches[2])
   end
 
   -- Reload all the plugins!
-  if matches[1] == 'reload' then
+  if matches[1] == '*' then
     return reload_plugins(true)
   end
 end
@@ -171,11 +171,11 @@ return {
     "/plugins reload: reloads all plugins." },
   patterns = {
     "^/plugins$",
-    "^/plugins? (enable) ([%w_%.%-]+)$",
-    "^/plugins? (disable) ([%w_%.%-]+)$",
-    "^/plugins? (enable) ([%w_%.%-]+) (chat)",
-    "^/plugins? (disable) ([%w_%.%-]+) (chat)",
-    "^/plugins? (reload)$" },
+    "^/plugins? (+) ([%w_%.%-]+)$",
+    "^/plugins? (-) ([%w_%.%-]+)$",
+    "^/plugins? (+) ([%w_%.%-]+) (chat)",
+    "^/plugins? (-) ([%w_%.%-]+) (chat)",
+    "^/plugins? (*)$" },
   run = run,
   privileged = true
 }
